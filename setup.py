d1295ba73d035e9b0c7c5998bacde47776919d75
Paul Traylor <paul@wegame.com>
2009-12-19 15:26:08 -0800

    Simple setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name='AnimeNFO',
+	description='AnimeNFO Radio Notifier',
+	author='Paul Traylor',
+	url='http://github.com/kfdm/radio-growl',
+	version='0.1',
+	py_modules=['AnimeNFO'],
+	)
