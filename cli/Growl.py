64e8c42d5c2382cac17af95b41f3e2d7aa28bed5
Paul Traylor <kungfudiscomonkey@gmail.com>
2011-12-23 15:40:59 -0800

    Remove dependency on pydefaults

--- cli/Growl.py
+++ cli/Growl.py
@@ -1,30 +1,26 @@
-import pydefaults
 import gntp.notifier
 import logging
 
-radio_settings = pydefaults.database('com.github.kfdm.radio')
 
 class GrowlNotifier(gntp.notifier.GrowlNotifier):
 	def __init__(self):
-		self.applicationName = radio_settings['appname']
-		self.notifications = [radio_settings['title']]
-		self.defaultNotifications = self.notifications
-		self.applicationIcon = radio_settings['icon']
-		self.hostname = radio_settings['host']
-		self.password = radio_settings['password']
-		self.port = int(radio_settings['port'])
-		self.debug = radio_settings['debug']
-		
+		gntp.notifier.GrowlNotifier.__init__(
+			self,
+			applicationName='AnimeNFO Radio',
+			notifications=['Now Playing'],
+			applicationIcon='http://www.animenfo.com/favicon.ico',
+		)
+
 		try:
 			self.register()
 		except:
 			logging.getLogger(__name__).exception('Is growl running ? Exiting....')
 			exit()
-			
-	def alert(self,title,message,image):
+
+	def alert(self, title, message, image):
 		try:
 			self.notify(
-				noteType=radio_settings['title'],
+				noteType='Now Playing',
 				title=title,
 				description=message,
 				icon=image,
