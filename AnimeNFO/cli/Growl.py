947980b6d712e8a531707292f33ff58739ecb4e6
Paul Traylor <kungfudiscomonkey@gmail.com>
2012-02-05 17:56:42 -0800

    Switch to using an image cache and sending the notification image as data

--- AnimeNFO/cli/Growl.py
+++ AnimeNFO/cli/Growl.py
@@ -1,10 +1,12 @@
-import gntp.notifier
+import gntp.config
 import logging
+from clint import resources
+import urllib2
 
 logger = logging.getLogger(__name__)
 
 
-class GrowlNotifier(gntp.notifier.GrowlNotifier):
+class GrowlNotifier(gntp.config.GrowlNotifier):
 	def __init__(self):
 		gntp.notifier.GrowlNotifier.__init__(
 			self,
@@ -12,6 +14,8 @@ class GrowlNotifier(gntp.notifier.GrowlNotifier):
 			notifications=['Now Playing'],
 			applicationIcon='http://www.animenfo.com/favicon.ico',
 		)
+
+		resources.init('kfdm', 'radio-growl')
 
 		try:
 			self.register()
@@ -23,12 +27,26 @@ class GrowlNotifier(gntp.notifier.GrowlNotifier):
 		pass
 
 	def alert(self, title, message, image, callback):
+		data = None
+		image_path = None
+		image_name = image.split('/').pop()
+		with resources.cache.open(image_name, 'w+') as f:
+			logger.info('Opening %s', f.name)
+			image_path = f.name
+			data = f.read()
+			if len(data) is 0:
+				logger.info('Downloading: %s', image)
+				data = urllib2.urlopen(image).read()
+				f.write(data)
+			else:
+				data = f.read()
+
 		try:
 			self.notify(
 				noteType='Now Playing',
 				title=title,
 				description=message,
-				icon=image,
+				icon=data,
 				callback=callback,
 				)
 		except:
